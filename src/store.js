e4707dcae172ba54b22cad7d45b0d9add21785d5
Tatsuya <yokokura0413@gmail.com>
2021-03-02 07:29:19 +0900

    mapState, mapGetters

--- src/store.js
+++ src/store.js
@@ -4,7 +4,44 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodoCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,65 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>{{ userName }}</h1>
+    <!-- <p>This event was created by {{ userName }}</p>
+    <p>This event was created by {{ userId }}</p> -->
+    
+    <!-- 【3rd step】 -->
+    <p>This event was created by {{ user.name }}</p>
+    <p>This event was created by {{ user.id }}</p>
+    <p>{{ catLength }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>{{ getEvent(1) }}</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  // 【1st step】
+  // computed: {
+  //   userName() {
+  //     return this.$store.state.user.name
+  //   },
+  //   userId() {
+  //     return this.$store.state.user.id
+  //   }
+  // }
+
+  // computed: mapState({
+  //   【2nd step】
+  //   userName: state => state.user.name,
+  //   userId: state => state.user.id,
+  //   categories: state => state.categories
+
+  //   【3rd step】
+  //   user: 'user',
+  //   categories: 'categories'
+  // })
+
+  // 【4th step】
+  // computed: mapState(['user', 'categories'])
+
+  // 【5th step】
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+
+    // 動的にgettersを呼び出す時は、この方法以外でmapGettersを使うと便利
+    getEvent() {
+      return this.$store.getters.getEventById
+    },
+
+    ...mapState(['user', 'categories']),
+    ...mapGetters(['getEventById'])
+  }
+}
+</script>
+
+<style scoped>
+</style>
