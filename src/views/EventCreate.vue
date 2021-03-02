abd130115e87f287e84a250c092e770fb0260193
Tatsuya <yokokura0413@gmail.com>
2021-03-03 06:42:56 +0900

    actionとmutationの実装

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -3,7 +3,7 @@
     <h1>{{ userName }}</h1>
     <!-- <p>This event was created by {{ userName }}</p>
     <p>This event was created by {{ userId }}</p> -->
-    
+
     <!-- 【3rd step】 -->
     <p>This event was created by {{ user.name }}</p>
     <p>This event was created by {{ user.id }}</p>
@@ -13,11 +13,56 @@
     </ul>
     <p>{{ getEvent(1) }}</p>
     <p>{{ getEventById(2) }}</p>
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date" />
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex'
+import Datepicker from 'vuejs-datepicker'
 
 export default {
   // 【1st step】
@@ -57,9 +102,60 @@ export default {
 
     ...mapState(['user', 'categories']),
     ...mapGetters(['getEventById'])
+  },
+
+  components: {
+    Datepicker
+  },
+  data() {
+    const times = []
+    for (let i = 0; i < 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      times,
+      categories: this.$store.state.categories,
+      event: this.createFreshEventObject()
+    }
+  },
+  methods: {
+    createEvent() {
+      this.$store
+        .dispatch('createEvent', this.event)
+        .then(() => {
+          this.$router.push({
+            name: 'event-show',
+            params: { id: this.event.id }
+          })
+          this.event = this.createFreshEventObject()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    createFreshEventObject() {
+      const user = this.$store.state.user
+      const id = Math.floor(Math.random() * 1000000)
+
+      return {
+        id: id,
+        user: user,
+        category: '',
+        organizer: user,
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
+.field {
+  margin-bottom: 24px;
+}
 </style>
