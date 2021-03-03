0b7e24e43f9952148fe22c49e32fa7f2f95a43c1
Tatsuya <yokokura0413@gmail.com>
2021-03-04 08:29:47 +0900

    カードコンポーネントの作成

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <h1>Event listing</h1>
-    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
-      >Show event #1</router-link
-    >
+    <router-link :to="{ name: 'event-show', params: { id: 1 } }">
+      <EventCard />
+    </router-link>
   </div>
 </template>
 
 <script>
-export default {}
+import { EventCard } from '@/components'
+
+export default {
+  components: {
+    EventCard
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
