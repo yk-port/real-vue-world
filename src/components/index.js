02c412783a65af1e01f1f47eec0bd25fff0ed059
Tatsuya <yokokura0413@gmail.com>
2021-03-05 07:04:38 +0900

    slotの導入

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as EventCard } from '@/components/EventCard.vue'
 export { default as NavBar } from '@/components/NavBar.vue'
+export { default as BaseIcon } from '@/components/BaseIcon.vue'
