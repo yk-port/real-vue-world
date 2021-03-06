3be7c314853ad2a13026df401739210485c927af
Tatsuya <yokokura0413@gmail.com>
2021-03-06 10:39:56 +0900

    axiosでAPI通信

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Event listing</h1>
     <router-link :to="{ name: 'event-show', params: { id: 1 } }">
-      <EventCard />
+      <EventCard v-for="event in events" :key="event.id" :event="event" />
     </router-link>
   </div>
 </template>
@@ -10,9 +10,34 @@
 <script>
 import { EventCard } from '@/components'
 
+// 直接axiosを呼び出す方法
+// import axios from 'axios'
+
+// 別ファイルからaxiosインスタンスを呼び出す方法
+import EventService from '@/services/EventService.js'
+
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    // 直接axiosを呼び出す方法
+    // axios
+    //   .get('http://localhost:3000/events')
+
+    // 別ファイルからaxiosインスタンスを呼び出す方法
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
   }
 }
 </script>
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
     <!-- $route.params.id -->
-    <h1>Showing event #{{ id }}</h1>
+    <h1>Showing event {{ event.title }}</h1>
   </div>
 </template>
 
 <script>
+// 別ファイルからaxiosインスタンスを呼び出す方法
+import EventService from '@/services/EventService.js'
+
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
+  }
 }
 </script>
 
