349a9c6cb7cf2d55cf8acfda8cb6a836fd353ea9
Tatsuya <yokokura0413@gmail.com>
2021-05-12 07:43:28 +0900

    add: mapGetters

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: { id: 'abc123', name: 'Adam Jahr' },
+    user: { id: 1, name: 'Adam Jahr' },
     categories: [
       'sustainability',
       'nature',
@@ -15,8 +15,30 @@ export default new Vuex.Store({
       'food',
       'community',
     ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false },
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' },
+    ],
   },
   mutations: {},
   actions: {},
+  getters: {
+    catLength: (state) => state.categories.length,
+    doneTodos: (state) => state.todos.filter((todo) => todo.done),
+    activeTodosCount: (state) => {
+      return state.todos.filter((todo) => !todo.done).length
+    },
+    getEventById: (state) => (id) => {
+      return state.events.find((event) => event.id === id)
+    },
+  },
   modules: {},
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,14 +2,23 @@
   <div>
     <h1>Create an Event, {{ user.name }}</h1>
     <p>This event is created by {{ user.id }}</p>
+    <p>{{ getEventById(user.id) }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
-  computed: mapState(['user', 'categories']),
+  computed: {
+    ...mapState(['user', 'categories']),
+    ...mapGetters([
+      'catLength',
+      'doneTodos',
+      'activeTodosCount',
+      'getEventById',
+    ]),
+  },
 }
 </script>
 
