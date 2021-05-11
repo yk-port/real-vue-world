9cef7899b4249c8c141601df654e583157d170e2
Tatsuya <yokokura0413@gmail.com>
2021-05-12 07:25:45 +0900

    add: mapState

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <h2>This is Create Page</h2>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.id }}</p>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState(['user', 'categories']),
+}
 </script>
 
 <style lang="scss"></style>
