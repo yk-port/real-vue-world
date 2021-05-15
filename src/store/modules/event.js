fd28cef552323fe83be7850819a6af51e33c2cf1
Tatsuya <yokokura0413@gmail.com>
2021-05-15 15:00:50 +0900

    310-finish-modules

---
+++ src/store/modules/event.js
@@ -0,0 +1,62 @@
+import EventService from '@/services/EventService.js'
+
+export const namespaced = true
+
+export const state = {
+  events: [],
+  eventsTotal: 0,
+  event: {},
+}
+
+export const mutations = {
+  ADD_EVENT(state, event) {
+    state.events.push(event)
+  },
+  SET_EVENTS(state, events) {
+    state.events = events
+  },
+  SET_EVENT_TOTAL(state, eventsTotal) {
+    state.eventsTotal = eventsTotal
+  },
+  SET_EVENT(state, event) {
+    state.event = event
+  },
+}
+
+export const actions = {
+  createEvent({ commit }, event) {
+    return EventService.postEvent(event).then(() => {
+      commit('ADD_EVENT', event)
+    })
+  },
+  fetchEvents({ commit }, { perPage, page }) {
+    return EventService.getEvents(perPage, page)
+      .then((response) => {
+        commit('SET_EVENT_TOTAL', parseInt(response.headers['x-total-count']))
+        commit('SET_EVENTS', response.data)
+      })
+      .catch((error) => console.log('There was an error:', error.response))
+  },
+  fetchEvent({ commit, getters }, id) {
+    const event = getters.getEventById(id)
+
+    if (event) {
+      commit('SET_EVENT', event)
+    } else {
+      EventService.getEvent(id)
+        .then((response) => commit('SET_EVENT', response.data))
+        .catch((error) => console.log('There was an error:', error.response))
+    }
+  },
+}
+
+export const getters = {
+  catLength: (state) => state.categories.length,
+  doneTodos: (state) => state.todos.filter((todo) => todo.done),
+  activeTodosCount: (state) => {
+    return state.todos.filter((todo) => !todo.done).length
+  },
+  getEventById: (state) => (id) => {
+    return state.events.find((event) => event.id === id)
+  },
+}
